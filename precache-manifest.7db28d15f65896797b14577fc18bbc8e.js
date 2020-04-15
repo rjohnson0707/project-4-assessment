bb15a54a7f326d4aeb95c039926650bea55d4b99
rjohnson0707 <rwilljohnson@icloud.com>
2020-04-15 15:03:25 -0600

    Updates

---
+++ precache-manifest.7db28d15f65896797b14577fc18bbc8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c21e829235fc0f7983ddb10ae3b8e02",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "fff44841ab58a513f569",
+    "url": "/project-4-assessment/static/css/main.72dab6d0.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fff44841ab58a513f569",
+    "url": "/project-4-assessment/static/js/main.2620a4a0.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
